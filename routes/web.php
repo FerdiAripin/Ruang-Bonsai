6ce79f552c5d0b531f5204b0d5fbb365b54b5e24
FerdiAripin <19231901@bsi.ac.id>
2025-04-13 15:36:33 +0700

    add default user Customer

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
 
 Route::view('/', 'welcome');
 
@@ -12,4 +14,12 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/admin-login', function () {
+    if (Auth::check() && Auth::user()->roles == 'Admin') {
+        return redirect('/admin');
+    }
+
+    return redirect('/dashboard');
+})->middleware(['auth']);
